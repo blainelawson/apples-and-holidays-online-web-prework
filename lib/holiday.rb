3b97c416082f40a55313b209e97a28408bd6fbd9
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-14 19:38:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  new_array = Array.new
+  # new_array = Array.new
   
   # holiday_hash.collect do |season, holidays|
   
@@ -90,6 +90,12 @@ def all_holidays_with_bbq(holiday_hash)
   # end
   
   # new_array
+  
+  holidays_has.collect do |season, holidays|
+      holidays.collect do |holiday, supply|
+        if supply.
+  
+  
 end
 
 
